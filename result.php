74a7c70cec10011c26e5eda2d4b6816de7a3d10f
Luca10992 <lucalillo.99.1@gmail.com>
2024-02-26 15:15:32 +0100

    first commit

---
+++ result.php
@@ -0,0 +1,62 @@
+<?php require_once "./main.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./icon-bear.png">
+    <title>PHP Hotel</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+
+    <div class="container my-5">
+        <form method="GET">
+            <select name="parking" id="parking">
+                <option selected="Parking">Parking</option>
+                <option value="Si">Si</option>
+                <option value="No">No</option>
+            </select>
+            <select name="vote" id="vote">
+                <option selected="vote">Vote</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </select>
+            <button type="submit"class="btn btn-primary">Search</button>
+        </form>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Parking</th>
+                    <th>Vote</th>
+                    <th>Distance to center</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php foreach($hotels as $hotel): ?>
+                    <tr>
+                        <td><?= $hotel["name"] ?></td>
+                        <td><?= $hotel["description"] ?></td>
+                        <td><?= $hotel["parking"] ?></td>
+                        <td><?= $hotel["vote"] ?></td>
+                        <td><?= $hotel["distance_to_center"] ?></td>
+                    </tr>
+
+
+                <?php endforeach; ?>
+
+            </tbody>
+        </table>
+    </div>
+    
+</body>
+</html>
